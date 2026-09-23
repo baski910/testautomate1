cf82f42babd8b5a3b989127c82d1340cb75534e7
baski910 <baskar910@gmail.com>
2026-09-23 01:01:27 +0000

    public key modified in main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "linode_instance" "jenkins_vm" {
   image           = "linode/ubuntu22.04"
   region          = var.region
   type            = var.instance_type
-  authorized_keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE06KaZ0EDcCMS8Hf2ItnOrIbmMyAolJjoWXlTma0hrw admin@localhost"]
+  authorized_keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMenziMZ+/12c6aXda0qJKVY/pV+F4iW02gyB4HTa6Wr admin@localhost"]
 }
 
 output "vm_public_ip" {
