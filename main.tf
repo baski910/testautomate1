e78d61ef2e10681b9cb3cbcd3c9eabc5b4772e74
baski910 <baskar910@gmail.com>
2026-09-23 01:23:19 +0000

    updated main.tf for debian vm

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
-resource "linode_instance" "jenkins_vm" {
-  label           = "jenkins-provisioned-vm"
-  image           = "linode/ubuntu22.04"
-  region          = var.region
-  type            = var.instance_type
-  authorized_keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMenziMZ+/12c6aXda0qJKVY/pV+F4iW02gyB4HTa6Wr admin@localhost"]
+resource "linode_instance" "debian_vm" {
+  label           = "debian-13-2gb-vm"
+  region          = "us-east" # Change to your preferred region (e.g., ap-south, eu-west)
+  type            = "g6-standard-1" # 2 GB RAM, 1 vCPU, 50 GB Disk
+  image           = "linode/debian13"
+  root_pass       = random_password.root_pass.result
+  authorized_keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMenziMZ+/12c6aXda0qJKVY/pV+F4iW02gyB4HTa6Wr admin@localhost"] # Optional but recommended
 }
 
 output "vm_public_ip" {
